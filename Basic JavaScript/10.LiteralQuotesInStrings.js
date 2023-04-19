e5299213781f62001ee980741864787b3d19c629
Elizaveta Kritskaya <post.kri@gmail.com>
2023-04-19 22:05:28 +0300

    add \"...\"

---
+++ Basic JavaScript/10.LiteralQuotesInStrings.js
@@ -0,0 +1,3 @@
+const sampleStr = "Alan said, \"Peter is learning JavaScript\".";
+// Alan said, "Peter is learning JavaScript".
+const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
