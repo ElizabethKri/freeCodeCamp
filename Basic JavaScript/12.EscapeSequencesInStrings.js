216b992a2c1a000a1fb20f58789010b56b64471e
Elizaveta Kritskaya <post.kri@gmail.com>
2023-04-21 21:32:37 +0300

    add newline and tab

---
+++ Basic JavaScript/12.EscapeSequencesInStrings.js
@@ -0,0 +1,9 @@
+// \'	single quote
+// \"	double quote
+// \\	backslash
+// \n	newline
+// \t	tab
+// \r	carriage return
+// \b	backspace
+// \f	form feed
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"
