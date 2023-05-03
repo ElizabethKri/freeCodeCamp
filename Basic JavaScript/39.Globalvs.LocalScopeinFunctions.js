356f8ad9d558bec8027133c1861d8a34e9f7298f
Elizaveta Kritskaya <post.kri@gmail.com>
2023-05-03 22:16:48 +0300

    add function and local&global scope

---
+++ Basic JavaScript/39.Globalvs.LocalScopeinFunctions.js
@@ -0,0 +1,11 @@
+function myLocalScope(){
+    //Only change code below this line
+    const myVar = "loc";
+    console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+console.log('inside myLocalScope', myVar);
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
