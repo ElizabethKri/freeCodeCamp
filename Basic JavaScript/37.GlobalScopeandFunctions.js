0fc8ea6004a97d75dd170d5c38bff7e7e3f19b00
Elizaveta Kritskaya <post.kri@gmail.com>
2023-05-03 22:12:15 +0300

    add function and global scope

---
+++ Basic JavaScript/37.GlobalScopeandFunctions.js
@@ -0,0 +1,14 @@
+const myGlobal = 10;
+function fun1(){
+    oopsGlobal=myGlobal - 5
+}
+function fun2(){
+    let output="";
+    if(typeof myGlobal !="undefined"){
+        output+="myGlobal:"+ myGlobal;
+    }
+    if (typeof oopsGlobal != "undefined") {
+        output += "oopsGlobal:" + oopsGlobal;
+    }
+    console.log(output);
+}
